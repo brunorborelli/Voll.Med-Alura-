6d8f3092f7c3593c0dc90fb7ebf57779597e4c3e
Bruno Borelli <rodriguesborellibruno@gmail.com>
2023-03-13 20:53:16 -0300

    Added db conection;medico and endereco entity, medicoRepository + new dependencies

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,22 +1,28 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.medico;
+import med.voll.api.medico.medicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
     
+    @Autowired
+    private medicoRepository repository;
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
 
-        System.out.println(dados);
+        repository.save(new medico(dados));
             
-        
 
     }
 
