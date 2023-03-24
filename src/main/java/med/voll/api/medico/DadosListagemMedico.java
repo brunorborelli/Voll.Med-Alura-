62b68bc22af2cfe43232c9d08b77b34c41690ebd
Bruno Borelli <rodriguesborellibruno@gmail.com>
2023-03-23 21:13:22 -0300

    feature/put_medico_added_in_controller

--- src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,9 +1,10 @@
 package med.voll.api.medico;
 
-public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
     
     public DadosListagemMedico(med.voll.api.medico.medico medico){
         this(
+         medico.getId(),
          medico.getNome(),
          medico.getEmail(), 
          medico.getCrm(), 
